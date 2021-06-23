a9eef29bdee214a77db3cfeb390816ddcaf87512
BattleWoofer <omeet100@gmail.com>
2021-06-22 20:50:54 -0500

    .

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 
-function click(){
+/*function click(){
     player.strings = player.strings.plus(1);
     document.getElementById('stringAmount').innerHTML = player.strings;
     window.alert(player.strings);
-}
+}*/
 
 /*function updateString{
     var element = document.getElementById("stringAmount");
